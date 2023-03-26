ab366606626c2417a61220d9e4231bd76f9fb946
Stanislav Romanov <kaermorchen@gmail.com>
2023-03-26 09:10:43 +0300

    Remove unused code

--- src/grammar.ts
+++
@@ -1,9 +0,0 @@
-import regexp from 'escape-string-regexp';
-import LexerState from './enums/lexer-state.js';
-import type Reader from './reader.js';
-
-class Grammar {
-
-}
-
-export default Grammar;
