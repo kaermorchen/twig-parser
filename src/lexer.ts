80efcd07f7af3bebf115fa1a7954f3d853006eb2
Stanislav Romanov <kaermorchen@gmail.com>
2023-03-28 11:48:41 +0300

    Add punctuation tokens

--- src/lexer.ts
+++ src/lexer.ts
@@ -13,12 +13,22 @@ function createToken(config: ITokenConfig) {
 }
 
 createToken({ name: 'WhiteSpace', pattern: /\s+/, group: Lexer.SKIPPED });
-createToken({ name: 'Number', pattern: /[0-9]+(?:\.[0-9]+)?([Ee][\+\-][0-9]+)?/ });
-createToken({ name: 'String', pattern: /"([^#"\\]*(?:\\.[^#"\\]*)*)"|'([^'\\]*(?:\\.[^'\\]*)*)'/ });
+createToken({
+  name: 'Number',
+  pattern: /[0-9]+(?:\.[0-9]+)?([Ee][\+\-][0-9]+)?/,
+});
+createToken({
+  name: 'String',
+  pattern: /"([^#"\\]*(?:\\.[^#"\\]*)*)"|'([^'\\]*(?:\\.[^'\\]*)*)'/,
+});
 
 createToken({ name: 'LComment', pattern: /{#/ });
 createToken({ name: 'RComment', pattern: /#}/ });
-createToken({ name: 'Comment', pattern: /[\S\s]*?(?=\s*#})/, line_breaks: true, });
+createToken({
+  name: 'Comment',
+  pattern: /[\S\s]*?(?=\s*#})/,
+  line_breaks: true,
+});
 
 createToken({ name: 'LVariable', pattern: /{{[-~]?/ });
 createToken({ name: 'RVariable', pattern: /[-~]?}}/ });
@@ -29,16 +39,30 @@ createToken({ name: 'RBlock', pattern: /[-~]?%}/ });
 createToken({ name: 'True', pattern: /true/ });
 createToken({ name: 'False', pattern: /false/ });
 createToken({ name: 'Null', pattern: /null/ });
+createToken({ name: 'Dot', pattern: /\./ });
 createToken({ name: 'LCurly', pattern: /{/ });
 createToken({ name: 'RCurly', pattern: /}/ });
 createToken({ name: 'LSquare', pattern: /\[/ });
 createToken({ name: 'RSquare', pattern: /]/ });
+createToken({ name: 'LParen', pattern: /\(/ });
+createToken({ name: 'RParen', pattern: /\)/ });
 createToken({ name: 'Comma', pattern: /,/ });
 createToken({ name: 'Colon', pattern: /:/ });
+createToken({ name: 'SemiColon', pattern: /;/ });
 createToken({ name: 'Arrow', pattern: /=>/ });
+createToken({ name: 'Equals', pattern: /=/ });
+createToken({ name: 'Star', pattern: /\*/ });
+createToken({ name: 'Plus', pattern: /\+/ });
+createToken({ name: 'Question', pattern: /\?/ });
+createToken({ name: 'GreaterThan', pattern: />/ });
+createToken({ name: 'Slash', pattern: /\// });
+createToken({ name: 'VerticalBar', pattern: /\|/ });
 createToken({ name: 'Verbatim', pattern: /\bverbatim\b/ });
 createToken({ name: 'EndVerbatim', pattern: /\bendverbatim\b/ });
-createToken({ name: 'Name', pattern: /[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*/ });
+createToken({
+  name: 'Name',
+  pattern: /[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*/,
+});
 
 createToken({
   name: 'RawText',
