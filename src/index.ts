450d8a3e4f8859b02db55655d4931bf1d1546a95
Stanislav Romanov <kaermorchen@gmail.com>
2023-04-21 13:57:08 +0300

    Add walk to index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { TwigLexer } from './lexer.js'
 export { TwigParser } from './twig-parser.js'
-export { Walker } from './walker.js'
+export { Walker, walk } from './walker.js'
 export { parse } from './parse.js'
