080aea1ee8408fe566ffaa55dc6f313817559242
Stanislav Romanov <kaermorchen@gmail.com>
2023-05-01 12:42:15 +0300

    Add monaco editor

---
+++ website/app/components/monaco-editor.ts
@@ -0,0 +1,132 @@
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
+import {
+  Uri,
+  editor,
+  languages,
+} from 'monaco-editor/esm/vs/editor/editor.api.js';
+import { registerDestructor } from '@ember/destroyable';
+
+interface MonacoEditorSignature {
+  Args: {
+    readOnly?: boolean;
+    language: string;
+    value?: string;
+  };
+  Blocks: {
+    default: [];
+  };
+  Element: HTMLElement;
+}
+
+export default class MonacoEditorComponent extends Component<MonacoEditorSignature> {
+  editor: unknown;
+
+  // constructor(owner, args) {
+  //   super(owner, args);
+
+  //   this.args.invoker?.subscribe(this);
+  // }
+
+  @action
+  initEditor(el: HTMLElement) {
+    this.editor = editor.create(el, {
+      readOnly: this.args.readOnly,
+      language: this.args.language,
+      // wordWrap: 'on',
+      // wrappingIndent: 'same',
+      lineNumbers: 'off',
+      roundedSelection: false,
+      scrollBeyondLastLine: false,
+      minimap: {
+        enabled: false,
+      },
+      scrollbar: {
+        alwaysConsumeMouseWheel: false,
+      },
+      value: this.args.value,
+    });
+
+    // if (Array.isArray(this.args.actions)) {
+    //   this.args.actions.forEach((item) => {
+    //     // assign because item can be a proxy object
+    //     this.editor.addAction(Object.assign({}, item));
+    //   });
+    // }
+
+    // Autoresize height of element
+    // const minHeight = this.args.minHeight ?? 160;
+    // const maxHeight = this.args.maxHeight ?? 420;
+    // const onDidContentSizeChangeHandler = this.editor.onDidContentSizeChange(
+    //   () => {
+    //     const contentHeight = Math.min(
+    //       Math.max(this.editor.getContentHeight(), minHeight),
+    //       maxHeight
+    //     );
+
+    //     el.style.height = `${contentHeight + 1}px`;
+    //     this.editor.layout();
+    //   }
+    // );
+    // registerDestructor(this, onDidContentSizeChangeHandler.dispose);
+
+    // // onDidChangeModelContent
+    // if (this.args.onDidChangeModelContent) {
+    //   const onDidChangeModelContent = this.editor.onDidChangeModelContent(
+    //     () => {
+    //       this.args.onDidChangeModelContent(
+    //         this.editor.getValue(),
+    //         this.editor
+    //       );
+    //     }
+    //   );
+    //   registerDestructor(this, onDidChangeModelContent.dispose);
+    // }
+
+    // // Validation
+    // if (this.args.onDidValidation) {
+    //   const onDidChangeMarkersHandler = editor.onDidChangeMarkers(() => {
+    //     const model = this.editor.getModel();
+
+    //     this.args.onDidValidation(
+    //       editor
+    //         .getModelMarkers({ resource: model.uri })
+    //         .map((item) => item.message),
+    //       this.editor
+    //     );
+    //   });
+    //   registerDestructor(this, onDidChangeMarkersHandler.dispose);
+    // }
+  }
+
+  // @action
+  // updateValue() {
+  //   const model = this.editor.getModel();
+
+  //   if (model.uri.toString() !== this.args.uri) {
+  //     const newModel = getModel(
+  //       this.args.value,
+  //       this.args.language,
+  //       this.args.uri,
+  //       this.args.schema
+  //     );
+
+  //     this.editor.setModel(newModel);
+  //     model.dispose();
+  //   } else {
+  //     this.editor.setValue(this.args.value);
+  //   }
+  // }
+
+  // @action
+  // invokeSendValue() {
+  //   this.args.sendValue?.(this.editor.getValue(), this.editor);
+  // }
+
+  willDestroy() {
+    // this.editor.getModel()?.dispose();
+    // this.editor.dispose();
+
+    super.willDestroy();
+  }
+}
--- website/ember-cli-build.js
+++ website/ember-cli-build.js
@@ -1,6 +1,11 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+function isProduction() {
+  return EmberApp.env() === 'production';
+}
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
@@ -15,15 +20,33 @@ module.exports = function (defaults) {
       },
     ],
     packagerOptions: {
+      publicAssetURL: isProduction() ? EmberApp.env().rootURL : '/',
+      cssLoaderOptions: {
+        // don't create source maps in production
+        sourceMap: isProduction() === false,
+        // enable CSS modules
+        modules: {
+          // global mode, can be either global or local
+          // we set to global mode to avoid hashing tailwind classes
+          mode: 'global',
+          // class naming template
+          localIdentName: isProduction()
+            ? '[sha512:hash:base64:5]'
+            : '[path][name]__[local]',
+        },
+      },
       webpackConfig: {
         module: {
           rules: [
+            { test: /\.ttf$/, type: 'asset/resource' },
             {
               test: /\.css$/i,
+              exclude: /node_modules/,
               use: [
                 {
                   loader: 'postcss-loader',
                   options: {
+                    sourceMap: isProduction() === false,
                     postcssOptions: {
                       config: 'config/postcss.config.js',
                     },
@@ -33,6 +56,11 @@ module.exports = function (defaults) {
             },
           ],
         },
+        plugins: [
+          new MonacoWebpackPlugin({
+            languages: ['json'],
+          }),
+        ],
       },
     },
   });
