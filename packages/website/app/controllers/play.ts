893505215ab8329186b4c0bb0ce75e0816d058c8
Stanislav Romanov <kaermorchen@gmail.com>
2023-05-04 14:25:57 +0300

    Add menu links

--- packages/website/app/controllers/play.ts
+++ packages/website/app/controllers/play.ts
@@ -1,11 +1,14 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { parse } from 'twig-parser';
 
 export default class PlayController extends Controller {
   @tracked
   code = 'hello';
 
   get ast() {
-    return this.code;
+    const { ast } = parse(this.code);
+
+    return JSON.stringify(ast, null, 2);
   }
 }
