05a2ee5170d7db16808913b0a3d0906b7dedd531
Stanislav Romanov <kaermorchen@gmail.com>
2023-05-05 09:48:01 +0300

    Remove index route

--- website/app/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-// import { service } from '@ember/service';
-
-export default class IndexRoute extends Route {
-  // @service router;
-  // beforeModel(): void | Promise<unknown> {
-  //   this.router.transitionTo('play');
-  // }
-}
